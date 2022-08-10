58ac1aa5b5f618fe7a72b9b79df8e75eb7fcbaab
queuing4oranges <katjazenker@gmail.com>
2022-08-10 22:16:27 +0200

    navbar + styling

---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+import './Navbar.css';
+import { Link } from 'react-router-dom';
+
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div className='navbar'>
+        <nav>
+        <Link to="/" className='brand'>
+            <h1>Cooking Katja</h1>
+        </Link >
+        <Link to="/create">
+            Create Recipe
+        </Link>
+        </nav>
+    </div>
+  )
+}
