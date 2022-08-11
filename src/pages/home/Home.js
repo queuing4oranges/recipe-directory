8c048f2cb8ed6de11b4b9ba256318d41615e0fda
queuing4oranges <katjazenker@gmail.com>
2022-08-11 22:09:05 +0200

    recipe component

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,9 +1,23 @@
 import './Home.css';
-
-import React from 'react'
+import React from 'react';
+import { useFetch } from '../../hooks/useFetch';
+import RecipeList from '../../components/RecipeList';
 
 export default function Home() {
+
+  const { data, isPending, error} =useFetch("http://localhost:3000/recipes")
+
+
   return (
-    <div>Home</div>
+    <div className='home'>
+
+    {error && <p className='error'>{error}</p>}
+    {isPending && <p className='loading'>Looking for recipes...</p>}
+    {data && <RecipeList recipes={data}/>
+    }
+
+
+
+    </div>
   )
 }
--- src/pages/recipe/Recipe.js
+++ src/pages/recipe/Recipe.js
@@ -1,9 +1,21 @@
 import './Recipe.css';
-
-import React from 'react'
+import React from 'react';
+import { useFetch } from '../../hooks/useFetch';
+import { useParams } from 'react-router-dom';
 
 export default function Recipe() {
+  const {id} = useParams()
+  const { data:recipe, isPending, error } = useFetch("http://localhost:3000/recipes/"+id)  
+
+  console.log(recipe)
+
   return (
-    <div>Recipe</div>
+    <div className='recipe'>
+      {error && <div className='error'>{error}</div>}
+      {isPending && <div className='loading'>Waiting...</div>}
+      {recipe && <h1>{recipe.title}</h1>}                        
+    </div>
+
   )
 }
+//doesnt need map fct since it's just ONE recipe!
